156fe4476b1af14bbc6a314f6637cec7e14b7331
Parousia <mmcg94@gmail.com>
2013-12-03 15:16:24 -0500

    Example output change

--- Infinite Space/src/net/parousia/InfiniteSpace/Game.java
+++ Infinite Space/src/net/parousia/InfiniteSpace/Game.java
@@ -3,7 +3,8 @@ package net.parousia.InfiniteSpace;
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.print("First change");
 
 	}
 
